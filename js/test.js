02bb3b1f439c0f3c95d44fabbbd221babc79e93e
Yuki Li <yol107@ucsd.edu>
2017-09-14 18:21:34 -0700

    Built first load template bone before animation

--- js/test.js
+++ js/test.js
@@ -1,12 +1,13 @@
 const main = function(){
-    if($(window).width()<600){
+    if($(window).width()<500){
         $('#nav').addClass('window_small');
+//        $('#nav').prepend("<img class='menu' src='./img/menu.png' alt='Menu' width='50px' height='50px'/>");
     }else{
         $('#nav').addClass('window_big');
     }
 
     $(window).on('resize', function() {
-        if($(window).width() < 600) {
+        if($(window).width() < 500) {
             $('#nav').removeClass('window_big');
             $('#nav').addClass('window_small');
         }else{
