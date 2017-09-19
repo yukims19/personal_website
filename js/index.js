929cbae66c4bc6f21d0fa4c75eb53c52b2b9dd64
Yuki Li <yol107@ucsd.edu>
2017-09-18 23:32:15 -0700

    Separated index js from home js

---
+++ js/index.js
@@ -0,0 +1,10 @@
+const main = function(){
+
+    //    $(window).load(function(){
+        window.setTimeout(function(){
+            window.location.href="./home.html";
+        },1500);
+//    });
+};
+
+$(document).ready(main);
